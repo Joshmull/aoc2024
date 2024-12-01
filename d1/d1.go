3090b40b26f487fa8c7868ddcbb8cc0a3e4ca64c
josh mullarkey <joshuamullarkey@protonmail.com>
2024-12-01 14:41:08 +0000

    Completed d1 part 1

--- d1/d1.go
+++ d1/d1.go
@@ -1,7 +1,59 @@
-package d1
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
-func Test(){
-    fmt.Println("test")
+func main(){
+    file, err := os.Open("./input")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    var left_list []int
+    var right_list []int
+    var diff int
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        string := strings.Split(scanner.Text(), "   ")
+        left_list = append(left_list, convert_num(string[0]))
+        right_list = append(right_list, convert_num(string[1]))
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    sort.Sort(sort.IntSlice(left_list))
+    sort.Sort(sort.IntSlice(right_list))
+
+    for i, _ := range left_list {
+        left := left_list[i]
+        right := right_list[i]
+
+        if left > right {
+            diff += left - right
+        } else {
+            diff += right - left
+        }
+    }
+
+    fmt.Println(diff)
 }
+
+func convert_num(str string) int {
+    num, err := strconv.Atoi(str)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    return num
+}
+
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-    "github.com/Joshmull/aoc2024/d1"
-)
-
-func main() { 
-    d1.Test()
-}
